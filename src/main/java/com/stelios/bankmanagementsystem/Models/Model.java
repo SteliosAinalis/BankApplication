c09855fc69b06e276f8b2c2e4c577ef52a1bf03a
Stelios <steliosain@hotmail.com>
2025-11-26 15:51:58 +0200

    working on controllers and viewfactory

--- src/main/java/com/stelios/bankmanagementsystem/Models/Model.java
+++ src/main/java/com/stelios/bankmanagementsystem/Models/Model.java
@@ -1,4 +1,20 @@
 package com.stelios.bankmanagementsystem.Models;
 
+import com.stelios.bankmanagementsystem.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    public Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+
+    public static synchronized Model getInstance() {
+        if(model == null){
+            model = new Model();
+        }
+        return model;
+    }
 }
--- src/main/java/com/stelios/bankmanagementsystem/Views/ViewFactory.java
+++ src/main/java/com/stelios/bankmanagementsystem/Views/ViewFactory.java
@@ -1,4 +1,53 @@
 package com.stelios.bankmanagementsystem.Views;
 
+import com.stelios.bankmanagementsystem.Controllers.Client.ClientController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ViewFactory {
+    private AnchorPane dashboardView;
+
+    public ViewFactory(){}
+
+    public AnchorPane getDashboardView() {
+        if(dashboardView == null){
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/Fxml/Client/Dashboard.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return dashboardView;
+    }
+
+    public void showLoginWindow(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        createStage(fxmlLoader);
+    }
+
+    public void showClientWindow(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Client.fxml"));
+        ClientController clientController = new ClientController();
+        fxmlLoader.setController(clientController);
+        createStage(fxmlLoader);
+
+
+    }
+
+    private void createStage(FXMLLoader fxmlLoader) {
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Bank");
+        stage.show();
+    }
+
+
 }
